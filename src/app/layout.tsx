d965106e891ac4a29b97d9ed2e72d5101d731338
Mayur Ramesh Mali <mayur1708mali@gmail.com>
2023-11-29 15:41:56 +0530

    Recipes json file create

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={inter.className}>
 				<nav>
-					<h1>Forms</h1>
+					<h1>Recipes for You</h1>
 				</nav>
 				{children}
 			</body>
